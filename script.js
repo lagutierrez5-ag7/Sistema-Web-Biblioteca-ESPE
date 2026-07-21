f98e131c51d368744b8580b3f677bb28497f2a30
lagutierrez5-ag7 <lagutierrez5@espe.edu.ec>
2026-07-21 15:12:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Lógica inicial para el Sistema Web Biblioteca ESPE
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Sistema de Biblioteca ESPE cargado correctamente.");
+});
